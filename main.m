b9a724e4963723ef90f07ebea1a9918d9163d2c9
Faiz-F <113477893+Faiz-F@users.noreply.github.com>
2022-10-02 21:30:43 -0400

    Update main.m

--- main.m
+++ main.m
@@ -3,9 +3,20 @@
 % fill the Excel sheet.
 
 clc; clear; close all;
+
 % The weight function takes in true/false argument for print figure and 
 % printing the iterations.
-Weight_est = weight_estimate(false,false);
+Weight_est1 = weight_estimate(false,false);
+
 % The cost function takes in arguments for Weight estimate and true/false 
 % for printing the required values.
-Cost_estimate = Cost_estimate(Weight_est,false);
+Cost_estimate = Cost_estimate(Weight_est1,false);
+
+% The preliminary sizing function takes in the 1st weight estimate and
+% outputs the Drag polars (true/false) and Constraints plots (true/false)
+[prelim_size,constraints] = Prelim_sizing(Weight_est1,false,false);
+
+% The second weight estimate function takes in 1st weight estimate and
+% TW_WS structs and true/false argument for printing the iterations counts.
+% It outputs the T-S plot for final prelim sizing.
+[TS_size,estimate2] = prelim_TS(Weight_est1,prelim_size,constraints);
