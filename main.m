c7bdd4e8bb5d824cf2301a67a7bedbc90335c5fa
Faiz-F <113477893+Faiz-F@users.noreply.github.com>
2022-10-02 22:25:05 -0400

    Update main.m

--- main.m
+++ main.m
@@ -20,3 +20,18 @@ Cost_estimate = Cost_estimate(Weight_est1,false);
 % TW_WS structs and true/false argument for printing the iterations counts.
 % It outputs the T-S plot for final prelim sizing.
 [TS_size,estimate2] = prelim_TS(Weight_est1,prelim_size,constraints);
+hold on
+%%
+E=struct('MTOW',get_weight(Weight_est1,prelim_size,1270,TS_size.S_design,38500,TS_size.T_design,true));
+
+S = linspace(800,2500,1000);
+T = linspace(0,750000,1000);
+fuel = zeros(numel(S),numel(S));
+for i=1:numel(S)
+    for j=1:numel(S)
+        fuel(i,j)=get_fuel_contours(prelim_size,S(i),T(j),TS_size.S_design,E.MTOW);
+    end
+end
+%%
+[X,Y]=meshgrid(S,T);
+contour(X,Y,fuel,'--k','ShowText','on')
